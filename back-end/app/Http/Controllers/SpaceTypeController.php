62a05d115f40445bc55a5630d40ea469af371e02
konich youssef <konichyoussef@gmail.com>
2024-05-07 04:17:28 +0100

    make infrastructure of youra project and make migrations files

---
+++ back-end/app/Http/Controllers/SpaceTypeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\space_type;
+use Illuminate\Http\Request;
+
+class SpaceTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(space_type $space_type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(space_type $space_type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, space_type $space_type)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(space_type $space_type)
+    {
+        //
+    }
+}
