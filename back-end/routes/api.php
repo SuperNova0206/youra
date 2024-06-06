30372fe57ec6ebaa000680b685c00a8451ea1c1a
konich youssef <konichyoussef@gmail.com>
2024-06-06 18:13:00 +0100

    updating front-end and the back-end

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MaterialsApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('materials', MaterialsApiController::class);
--- back-end/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route as route;
-use App\Http\Controllers\MaterialController;
-
-route::get('materials', [MaterialController::class, 'index']);
-route::post('materials', [MaterialController::class, 'insert']);
